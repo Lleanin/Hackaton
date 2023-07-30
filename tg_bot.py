2dafcf2a01d4961f94fe58439feacd6257c7a32a
sssadboy <Fifashf@yandex.ru>
2023-07-30 18:17:13 +0300

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -5,140 +5,134 @@ import telebot
 from dotenv import load_dotenv
 from telebot import types
 
-
 load_dotenv()
 tg_bot_token = os.environ.get("TG_BOT_TOKEN")
 bot = telebot.TeleBot(tg_bot_token)
-score = 0
 
-
-tasks_ru = ["Сколько всего тиов данных в Python?",
-            "Какое улучшение исправит код?:file_ext = filename.split('.')[-1]",
-            "Что делает метод append?",
-            "Что такое список?",
-            "Кортеж это изменяемый тип данных или нет?", 
-            "Для чего нужна последовательность '\n'?",
-            "Где правильно должны быть собраны import'ы по PEP8?",
-            "Time-это встроенная библиотека?",
-            "Как объявить функцию?",
-            "С какого числа начинается индексация?",
-            "Сколько элементов можно хранить в кортеже?",
-            "Будет ли работать этот код?:\n class = 'Основы Python'\n print(class)",
-            "Сколько библиотек можно импортировать в один проект?",
-            "Как получить данные от пользователя?",
-            "Какая библиотека отвечает за время?",
-            "Какая функция выводит что-либо в консоль?",
-            "Заполните пропуск в утверждении : Метод _______ умеет заменять одну часть строки на другую.",
-            "С помощью кавычек вы можете создавать _______, \n пример:'Devman'",
-            "Словарь – это объект, содержащий пары ключ-_______.",
-            "Метод _______ умеет подставлять переменные в строку.",
-            "_______ — это то, что вы передаёте функциям.",
-            "Списки могут содержать объекты любого типа: числа, строки, словари, другие списки.",
-            "Вы запустили код и получили KeyError. Какой метод словаря можно использовать, чтобы такой ошибки не случилось?",
-            "Есть два вида аргументов: _______ и именованные.",
-            "Функция sorted принимает на вход итерируемый объект и возвращает отсортированный список.",
-            "Пароли, токены и прочую чувствительную информацию необходимо сохранять в ____-файл.",
-            "Python лучше чем Java?)",
-            "Для чего нужна конструкция try/except?",
-            "Где правильно должны быть собраны функции по PEP8?",
-            "Что делает метод .upper()?",
-
-        ]   
-
-
-answers_ru = ["3",
-            "splitext из модуля os.path",
-            "Добавляет элемент в список?",
-            "Структура данных","Неизменяемый",
-            "Перевод курсора на следующую строку",
-            "В начале кода",
-            "Да",
-            "Через ключевое слово def",
-            "C 0",
-            "Неограниченное кол-во",
-            "Да",
-            "Неограниченное кол-во",
-            "Метод input()",
-            "time",
-            "print()",
-            ".replace()",
-            "Строки",
-            "Значение",
-            ".format()",
-            "Аргументы",
-            "Да",
-            ".get()",
-            "Позиционные",
-            "Да",
-            ".env",
-            "ДА",
-            "Для обработки исключений",
-            "После import'ов",
-            "Возращает копию строки, в которой все буквы сконвертированы к большому регистру"
-            ]
- 
+questions  = {
+    "Сколько всего типов данных в Python?": ["1", "2", "3", "4"],
     
+    "Какое улучшение исправит код?:\nfile_ext = filename.split('.')[-1]": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+
+    "Что делает метод append?": ["Добавляет элемент в список", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+
+    "Что такое список?": ["Структура данных", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"], 
+
+    "Кортеж это изменяемый тип данных или нет?": ["Неизменяемый", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+
+    "Для чего нужна последовательность '\n'?": ["Перевод курсора на следующую строку", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+
+    "Где правильно должны быть собраны import'ы по PEP8?": ["В начале кода", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Time-это встроенная библиотека?": ["Да", "Нет","Частично","Скорее да,чем нет"],
+    
+    "Как объявить функцию?": ["Через ключевое слово def", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "С какого числа начинается индексация?": ["C 0", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Сколько элементов можно хранить в кортеже?": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Будет ли работать этот код?:\n class = 'Основы Python'\n print(class)": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Сколько библиотек можно импортировать в один проект?": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Как получить данные от пользователя?": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Какая библиотека отвечает за время?": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Какая функция выводит что-либо в консоль?": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Заполните пропуск в утверждении : Метод _______ умеет заменять одну часть строки на другую.": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "С помощью кавычек вы можете создавать _______, \n пример:'Lleanin'": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Словарь – это объект, содержащий пары ключ-_______.": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Метод _______ умеет подставлять переменные в строку.": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "_______ — это то, что вы передаёте функциям.": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Списки могут содержать объекты любого типа: числа, строки, словари, другие списки.": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Вы запустили код и получили KeyError. Какой метод словаря можно использовать, чтобы такой ошибки не случилось?": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Есть два вида аргументов: _______ и именованные.": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Функция sorted принимает на вход итерируемый объект и возвращает отсортированный список.": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Пароли, токены и прочую чувствительную информацию необходимо сохранять в ____-файл.": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Python лучше чем Java?)": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Для чего нужна конструкция try/except?": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Где правильно должны быть собраны функции по PEP8?": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"],
+    
+    "Что делает метод .upper()?": ["splitext из модуля os.path", "split('.')[-1]", "split()[-1]", "split(filename)[-1]"]
+}
+
+
+modes = {
+    "5": 5,
+    "15": 15,
+    "30": 30,
+}
+
+
+user_scores = {}
+
+
 @bot.message_handler(commands=['start'])
-def start(message):
-
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn_1 = types.KeyboardButton("👋 Поздороваться")
-    markup.add(btn_1)
-    bot.send_message(message.from_user.id, "👋 Привет! Я твой бот-помошник!", reply_markup=markup)
+def handle_start(message):
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add("5 вопросов", "15 вопросов", "30 вопросов")
+    bot.send_message(message.chat.id, "Привет! Выбери режим:", reply_markup=markup)
 
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == '👋 Поздороваться':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn_ru = types.KeyboardButton("🇷🇺 Русский")
-        btn_en = types.KeyboardButton('🇬🇧 English')
-        markup.add(btn_ru, btn_en)
-        bot.send_message(message.from_user.id, "🇷🇺 Выберите язык / 🇬🇧 Choose your language", reply_markup=markup)
+# Обработчик выбора режима и запуск викторины
+@bot.message_handler(func=lambda message: message.text in modes.keys())
+def handle_mode_choice(message):
+    user_id = message.from_user.id
+    user_scores[user_id] = 0  # Обнуляем счет пользователя
+    num_questions = modes[message.text]
+    quiz_questions = random.sample(list(questions .keys()), num_questions)
+    ask_question(message, quiz_questions, 0)
 
-    if message.text == '🇷🇺 Русский':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Легкий режим(5 Вопросов)")
-        btn2 = types.KeyboardButton("Средний режим(15 Вопросов)")
-        btn3 = types.KeyboardButton("Тяжелый режим(30 Вопросов)")
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.from_user.id, "Выберите на какое кол-во вопросов вы собираетесь ответить:", reply_markup=markup)
 
-    elif message.text == "Легкий режим(5 Вопросов)":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        answers = []
-        Button_1 = types.KeyboardButton("5")
-        Button_2 = types.KeyboardButton(answers_ru[0])
-        Button_3 = types.KeyboardButton("2")
-        Button_4 = types.KeyboardButton("8")
-        markup.add(Button_1, Button_2, Button_3, Button_4)
-        bot.send_message(message.from_user.id, "1.{}:".format(tasks_ru[0]), reply_markup=markup)
-        answers.append(Button_1,Button_2,Button_3,Button_4)
+# Функция задает вопрос и предлагает варианты ответов
+def ask_question(message, quiz_questions, current_question):
+    chat_id = message.chat.id
+    question = quiz_questions[current_question]
+    answers = questions[question][:-1]
+    correct_answer = questions[question][-1]
 
-    if message.text == answers_ru[0]:
-        score+=1
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(*answers)
 
-    elif message.text == answers:
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        Button_1 = types.KeyboardButton("5")
-        Button_2 = types.KeyboardButton(answers_ru[1])
-        Button_3 = types.KeyboardButton("2")
-        Button_4 = types.KeyboardButton("8")
-        markup.add(Button_1, Button_2, Button_3, Button_4)
-        bot.send_message(message.from_user.id, "2.{}:", reply_markup=markup).format(tasks_ru[1])
+    bot.send_message(chat_id, f"{current_question + 1}. {question}", reply_markup=markup)
+    bot.register_next_step_handler(message, check_answer, quiz_questions, current_question, correct_answer)
 
-    elif message.text == "Средний режим(15 Вопросов)":
-        print("hello")
-    elif message.text == "Тяжелый режим(30 Вопросов)":
-        print("hello")
+# Функция проверяет ответ пользователя и переходит к следующему вопросу или выводит результат
+def check_answer(message, quiz_questions, current_question, correct_answer):
+    user_id = message.from_user.id
+    user_answer = message.text.strip()
 
-    elif message.text == 'en English':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Easy Mode (5 Questions)")
-        btn2 = types.KeyboardButton("Medium Mode (15 Questions)")
-        btn3 = types.KeyboardButton("Heavy Mode (30 Questions)")
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.from_user.id, "Выберите на какое кол-во вопросов вы собираетесь ответить:", reply_markup=markup)
-    
+    if user_answer == correct_answer:
+        user_scores[user_id] += 1
+
+    if current_question + 1 < len(quiz_questions):
+        ask_question(message, quiz_questions, current_question + 1)
+    else:
+        show_result(message, user_scores[user_id])
+
+# Функция выводит результат участника
+def show_result(message, score):
+    bot.send_message(message.chat.id, f"Твой результат:\nПравильные ответы: {score}/{len(user_scores)}")
+
+# Обработка всех остальных сообщений
+@bot.message_handler(func=lambda message: True)
+def handle_all_messages(message):
+    bot.reply_to(message, "Извини, я не понимаю. Выбери режим, чтобы начать викторину.")
+
 
 bot.polling(none_stop=True, interval=0)
